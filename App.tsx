b7b6ae36ce6bfd472658ca17274c622746af5c0f
ThanK <hbthanh582002@gmail.com>
2025-03-12 14:39:12 +0700

    unit#31: using View, Text, state and data-type

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState<number>(0);
+  const [person, setPerson] = useState({ name: "Person name", age: 10 });
   return (
     <View style={styles.container}>
+      <View>
+        <Text style={{ fontSize: 16, color: "red" }}>
+          This is the <Text style={styles.mainText}>{count}</Text>
+        </Text>
+        <Text>{JSON.stringify(person)}</Text>
+      </View>
       <Text>Open up App.tsx to start working on your Application!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -13,8 +20,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
+    paddingTop: 20,
+    paddingHorizontal: 12,
+  },
+  mainText: {
+    fontSize: 18,
+    fontWeight: "semibold",
+    color: "green",
   },
 });
